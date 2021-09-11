62c720adbd3c384a06fdec6e92b15c7560d15623
Nikita Shalimo <MegaFrostBall@gmail.com>
2021-09-12 02:58:43 +0300

    add application modules

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AppHeader = () => {
+    return (
+        <div>
+            <h1>Nikita Shalimo</h1>
+            <h2>5 posts, liked 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
