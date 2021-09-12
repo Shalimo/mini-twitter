c332685ae3a7e82d27a6ab8830de713cd46dcf0b
Nikita Shalimo <MegaFrostBall@gmail.com>
2021-09-12 14:30:55 +0300

    add css computed styles for components

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+import './app-header.css';
+
 const AppHeader = () => {
     return (
-        <div>
+        <div className="app-header d-flex">
             <h1>Nikita Shalimo</h1>
             <h2>5 posts, liked 0</h2>
         </div>
